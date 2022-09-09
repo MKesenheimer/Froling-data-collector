1b2ebac3446f6536d16d67a5e8d7f32f22a12a2a
Matthias Kesenheimer <m.kesenheimer@gmx.net>
2022-09-09 08:29:43 +0200

    Small corrections

--- restart.sh
+++ restart.sh
@@ -6,9 +6,10 @@ kill -9 $pid
 
 echo "Restarting data collector..."
 nohup ./run.sh > output.log 2> error.log &
+sleep 10
 
 pid=$(cat process.id)
-echo "Neww PID is $pid."
+echo "New PID is $pid."
 echo "Done."
 
 
